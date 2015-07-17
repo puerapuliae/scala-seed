d5b5a43c352e0e36d7ff24d37c8e6651c2637b2d
puerapuliae <bk@X-Wing.local>
2015-07-17 14:40:29 +0200

    refactored

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
+# Java Version
+JAVA_VERSION_MAJOR="8"
+JAVA_VERSION_MINOR="45"
+JAVA_VERSION_BUILD="14"
+JAVA_PACKAGE="jdk"
+JAVA_DIRECTORY="jdk1.${JAVA_VERSION_MAJOR}.0_${JAVA_VERSION_MINOR}"
+
 echo "Installing Oracle Java 8 ..."
-wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jdk-8u40-linux-x64.tar.gz"
-tar -xzvf jdk-8u40-linux-x64.tar.gz
+wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION_MAJOR}u${JAVA_VERSION_MINOR}-b${JAVA_VERSION_BUILD}/${JAVA_PACKAGE}-${JAVA_VERSION_MAJOR}u${JAVA_VERSION_MINOR}-linux-x64.tar.gz
+
+tar -xzvf jdk-${JAVA_VERSION_MAJOR}u${JAVA_VERSION_MINOR}-linux-x64.tar.gz
 mkdir /usr/lib/jvm
-mv jdk1.8.0_40 /usr/lib/jvm
-ln -s /usr/lib/jvm/jdk1.8.0_40/ /usr/lib/jvm/java-8-oracle
-update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/java-8-oracle/bin/java" 1
-update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/java-8-oracle/bin/javac" 1 
-rm /home/vagrant/jdk-8u40-linux-x64.tar.gz
+mv ${JAVA_DIRECTORY} /usr/lib/jvm
+ln -s /usr/lib/jvm/${JAVA_DIRECTORY} /usr/lib/jvm/java-${JAVA_VERSION_MAJOR}-oracle
+update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/java-${JAVA_VERSION_MAJOR}-oracle/bin/java" 1
+update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/java-${JAVA_VERSION_MAJOR}-oracle/bin/javac" 1 
+rm /home/vagrant/${JAVA_PACKAGE}-${JAVA_VERSION_MAJOR}u${JAVA_VERSION_MINOR}-linux-x64.tar.gz
 
 echo "Installing SBT ..."
 wget https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.7/sbt-launch.jar
@@ -20,3 +28,5 @@ java $SBT_OPTS -jar `dirname $0`/sbt-launch.jar "$@"
 EOF
 
 chmod 755 /bin/sbt
+
+echo "cd /vagrant" >> /home/vagrant/.bashrc
